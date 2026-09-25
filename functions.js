cbef6fb7006f8f3260330c6b97f42c468ff3bb95
Claudia Wavinya <mutungaclaudia5@gmail.com>
2026-09-25 22:46:16 +0300

    new file functions.js

---
+++ functions.js
@@ -0,0 +1,6 @@
+function calculateTax(amount) { return amount * 0.10; }
+function convertToUpperCase(text) { return text.toUpperCase(); }
+function findMaximum(num1, num2) { return num1 > num2 ? num1 : num2; }
+function isPalindrome(word) { return word === word.split('').reverse().join(''); }
+function calculateDiscountedPrice(originalPrice, discountPercentage) { return originalPrice - (originalPrice * (discountPercentage / 100)); }
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
